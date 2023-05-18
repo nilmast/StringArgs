40a8e3b76dc66b874a7451f2fd872acde634674c
Ujin <troi_daniel@mail.ru>
2023-05-18 13:27:28 +0300

    Add

---
+++ src/NoZeroDiv.java
@@ -0,0 +1,12 @@
+public class NoZeroDiv {
+    public static void main(String[] args) {
+        int result;
+        for (int i = -5; i < 6; i++) {
+            result = (i != 0) ? 100/i : 0;
+            if (i != 0) {
+                System.out.println("100/" + i + " равно " + result);
+            }
+
+        }
+    }
+}
